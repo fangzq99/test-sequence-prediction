b3c21402debc453e14b1a97a7a754dfcb13089e0
zq <fangziqiu123@gmail.com>
2023-03-19 20:43:18 +0800

    chore: set up project file structure

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// import { app, BrowserWindow } from "electron"
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
+
+  win.loadFile('./public/index.html')
+  // win.loadFile("index.html")
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
---
+++ src/renderer.js
@@ -0,0 +1,11 @@
+// Import the required modules
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+// Import the main App component
+const App = require("./index.js");
+
+console.log("Renderer process running.");
+
+// // Render the App component in the root element of the HTML page
+ReactDOM.render(<App />, document.getElementById("root"));
